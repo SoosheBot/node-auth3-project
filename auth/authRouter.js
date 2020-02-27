ef9ad7d8df10a0719d1c37c737bc05118adf25e5
sooshe <sooshe@sooshebot.com>
2020-02-27 13:57:21 -0600

    authrouter register created

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -5,3 +5,18 @@ const jwt = require("jsonwebtoken");
 const { jwtSecret } = require("../config/secrets");
 
 const Users = require("../users/usersModel");
+
+router.post("/register", (req,res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+
+    user.password = hash;
+
+    Users.add(user)
+    .then(saved => {
+        res.status(201).json(saved);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "Could not register user."})
+    });
+});
