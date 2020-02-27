1f7433109551fbf118f1c41d4c832ad7e36b2eca
sooshe <sooshe@sooshebot.com>
2020-02-27 13:47:47 -0600

    rewrite usersModel functions for clarity

--- users/usersModel.js
+++ users/usersModel.js
@@ -8,21 +8,26 @@ module.exports = {
 };
 
 function find() {
-    return db('users').select('id', 'username', 'password');
+    return db('usersTable').select('id', 'username', 'password');
   }
   
   function findBy(filter) {
-    return db('users').where(filter);
+    return db('usersTable').select('id', 'username', 'password').where(filter);
   }
-  
-  async function add(user) {
-    const [id] = await db('users').insert(user);
-  
-    return findById(id);
-  }
-  
+
   function findById(id) {
-    return db('users')
+    return db('usersTable')
       .where({ id })
       .first();
   }
+  
+ function add(user) {
+    return db("usersTable")
+    .insert(newUser)
+    .then(ids => {
+      const [id] = ids;
+      return findById(id);
+    });
+  }
+  
+  
